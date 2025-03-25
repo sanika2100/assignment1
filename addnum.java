cd99afa2e84fbddb763543b3d05334dfbb4e22ac
Sanika Pawar <sanikapawar8899@gmail.com>
2025-03-25 18:11:10 +0530

    Add files via upload

---
+++ addnum.java
@@ -0,0 +1,9 @@
+public class addnum {
+    public static void main(String[] args) {
+        int num1 = 88;
+        int num2 = 12;
+        int sum = num1 + num2;
+        
+        System.out.println("sum is: " + sum);
+    }
+}
